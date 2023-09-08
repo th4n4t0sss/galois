16cbacca8bb84d908013a7632f65899ed4a5d335
zeta3301 <136582783+zeta3301@users.noreply.github.com>
2023-09-08 07:03:29 +0400

    added SDL2_gfx for as flag

--- make.sh
+++ make.sh
@@ -4,8 +4,7 @@ set -xe
 CC="${CXX:-cc}"
 PKGS="sdl2"
 CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb"
-LIBS="-lm -lSDL2_ttf"
+LIBS="-lm -lSDL2_ttf -lSDL2_gfx"
 SRC="galois.c"
-#SRC="test.c"
 
 $CC $CFLAGS `pkg-config --cflags $PKGS` -o galois $SRC $LIBS `pkg-config --libs $PKGS`
