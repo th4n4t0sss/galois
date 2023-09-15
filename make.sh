d0e59000eeba2b050c1f5bfa228b5bf0415ec085
zeta3301 <gorxyz@proton.me>
2023-09-15 17:51:45 +0400

    removed -lm

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -xe
 
 CC="${CC:-cc}"
-PKGS="sdl2 SDL2_ttf" # SDL2_gfx"
+PKGS="sdl2 SDL2_ttf SDL2_gfx"
 CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb `pkg-config --cflags $PKGS`"
-LIBS="-lm `pkg-config --libs $PKGS`"
+LIBS="`pkg-config --libs $PKGS`"
 SRC="galois.c"
 
 $CC $CFLAGS -o galois $SRC $LIBS
