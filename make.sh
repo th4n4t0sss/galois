8c29d284056ed6b5b9dc4242e487ddc20d487706
zeta3301 <gorxyz@proton.me>
2023-09-15 05:25:53 +0400

    commented SDL_gfx

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 
 CC="${CC:-cc}"
-PKGS="sdl2 SDL2_ttf SDL2_gfx"
+PKGS="sdl2 SDL2_ttf" # SDL2_gfx"
 CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb `pkg-config --cflags $PKGS`"
 LIBS="-lm `pkg-config --libs $PKGS`"
 SRC="galois.c"
