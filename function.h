731a9ff9de62c8d7ee17d23add42720a3ba41eb2
zeta3301 <gorxyz@proton.me>
2023-09-27 02:13:53 +0400

    testing prime numbers

--- function.h
+++ function.h
@@ -1,6 +1,22 @@
 #include <math.h>
 
+/* testing prime numbers */
+bool is_prime(int n) {
+	if (n <= 1)
+		return false;
+	for (int i=2;i<=sqrt(n);++i) {
+		if (n % i == 0)
+            return false;
+	}
+	return true;
+}
+
+int primes(int x) {
+	if (is_prime(x))
+		return x;
+	return primes(x + 1);
+}
 /* TODO: text prompt for inserting mathematical function */
 double f(double x) {
-	return sqrt(x + pow(x, 2));
+	return x;
 }
